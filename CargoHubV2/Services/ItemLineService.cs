c08b5821e2b2d813adca04b6e5acfe05a7347fa7
nhojW <161321640+nhojW@users.noreply.github.com>
2025-01-16 18:41:35 +0100

    itemline changes

--- CargoHubV2/Services/ItemLineService.cs
+++ CargoHubV2/Services/ItemLineService.cs
@@ -15,9 +15,9 @@ namespace CargohubV2.Services
             _context = context;
         }
 
-        public async Task<List<Item_Line>> GetAllItemLinesAsync()
+        public async Task<List<Item_Line>> GetAllItemLinesAsync(int amount)
         {
-            return await _context.Items_Lines.Take(100).ToListAsync();
+            return await _context.Items_Lines.Take(amount).ToListAsync();
         }
 
         public async Task<Item_Line> GetItemLineByIdAsync(int id)
