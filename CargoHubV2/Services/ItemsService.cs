27bb2cbc3bd5fa0e25a4028fc014136d125961c3
Jyirdo <yordivdijk@gmail.com>
2025-01-21 11:11:00 +0100

    hotifx

--- CargoHubV2/Services/ItemsService.cs
+++ CargoHubV2/Services/ItemsService.cs
@@ -115,5 +115,18 @@ namespace CargohubV2.Services
             await _context.SaveChangesAsync();
             return true;
         }
+
+        public virtual async Task PopulateWeightInKgAsync()
+        {
+            var random = new Random();
+
+            var items = await _context.Items.ToListAsync();
+            foreach (var item in items)
+            {
+                item.WeightInKg = random.Next(1, 100);
+            }
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
